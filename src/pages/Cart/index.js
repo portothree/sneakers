8e364ff91bee792ae3fa586e5b1805aaf5573f88
Gustavo Porto <portothree@gmail.com>
2020-01-03 16:05:15 +0000

    :lipstick: styling cart page

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,69 @@
 import React from 'react';
 
+import './styles.scss';
+import productPhoto from '../../assets/beluga.jpg';
+import trashcan from '../../assets/trash-can.svg';
+
 export default function Cart() {
 	return (
-		<div>
-			<div></div>
-		</div>
+		<main className="order-details">
+			<table className="order-details__table">
+				<thead>
+					<tr>
+						<th></th>
+						<th>Product</th>
+						<th>Unit Price</th>
+						<th>Quantity</th>
+						<th>Total Price</th>
+						<th></th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>
+							<img
+								className="order-details__photo"
+								src={productPhoto}
+								alt="Gray sneaker"
+							/>
+						</td>
+						<td className="order-details__productname">
+							Yeezy Boost 350 V2 Beluga 2.0
+						</td>
+						<td>
+							<strong>€299,90</strong>
+						</td>
+						<td>
+							<label className="screen-reader-only" htmlFor="productqty">
+								Quantity
+							</label>
+							<input
+								className="order-details__qty"
+								id="productqty"
+								type="number"
+								value="3"
+							/>
+						</td>
+						<td>
+							<strong>€899,70</strong>
+						</td>
+						<td>
+							<button className="order-details__delete">
+								<img src={trashcan} alt="Trash Can" />
+							</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<div className="order-details__details">
+				<button className="order-details__checkout-btn">Checkout</button>
+				<div className="order-details__total">
+					<span>Total:</span>
+					<strong>€2699.1</strong>
+				</div>
+			</div>
+		</main>
 	);
 }
--- src/pages/Catalog/index.js
+++ src/pages/Catalog/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import cart from '../../assets/cart.svg';
 import productPhoto from '../../assets/beluga.jpg';
 
 import './style.scss';
@@ -10,6 +9,17 @@ export default function Catalog() {
 		<main className="catalog">
 			<h1 className="catalog__title">Catalog</h1>
 			<div className="catalog__items">
+				<div className="item">
+					<img className="item__photo" src={productPhoto} alt="Gray sneaker" />
+					<strong className="item__vendor">Adidas</strong>
+					<h2 className="item__title">Yeezy Boost 350 V2 Beluga 2.0</h2>
+					<span className="item__price">€299,90</span>
+
+					<button className="item__button" type="button">
+						<span className="item__qty">1</span>
+						<span className="item__action">Add to cart</span>
+					</button>
+				</div>
 				<div className="item">
 					<img className="item__photo" src={productPhoto} alt="Gray sneaker" />
 					<strong className="item__vendor">Adidas</strong>
